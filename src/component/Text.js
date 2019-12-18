0f090cbf183cc60f5283bd67b132241bde108eff
kevinchristianto <christiantok12@gmail.com>
2019-12-18 10:32:08 +0700

    Refreshing home

--- src/component/Text.js
+++ src/component/Text.js
@@ -7,7 +7,7 @@ export class Txt extends Component {
         const {style, ...withoutStyle} = this.props;
         return (
             // style below means combining 2 styles, one from this class and the withoutStyle is from rendered component
-            <Text style={[s.txt, style]} {...this.withoutStyle}>{this.props.children}</Text>
+            <Text {...withoutStyle} style={[s.txt, style]}>{this.props.children}</Text>
         );
     }
 }
@@ -16,7 +16,7 @@ export class TxtBold extends Component {
     render() {
         const {style, ...withoutStyle} = this.props;
         return (
-            <Text style={[s.txtBold, style]} {...this.withoutStyle}>{this.props.children}</Text>
+            <Text {...withoutStyle} style={[s.txtBold, style]}>{this.props.children}</Text>
         );
     }
 }
